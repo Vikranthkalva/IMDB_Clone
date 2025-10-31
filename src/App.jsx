94eb881b265f189bfd0b124cc098953a2c2d5933
Vikranthkalva <vikranthlucky123@gmail.com>
2025-10-31 20:48:20 +0530

    Switch Font Awesome to local import for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,35 @@ import Movies from './components/Movies'
 import Watchlist from './components/Watchlist'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Banner from './components/Banner'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 function App() {
 
   let [watchlist, setWatchlist] = useState([])
 
-let handleAddToWatchlist =(movieObj)=>{
-  let newWatchlist=[...watchlist, movieObj]
-  localStorage.setItem("moviesApp", JSON.stringify(newWatchlist))
-  setWatchlist(newWatchlist)
-console.log("Added to watchlist:", newWatchlist);
+  let handleAddToWatchlist = (movieObj) => {
+    let newWatchlist = [...watchlist, movieObj]
+    localStorage.setItem("moviesApp", JSON.stringify(newWatchlist))
+    setWatchlist(newWatchlist)
+    console.log("Added to watchlist:", newWatchlist);
 
-}
+  }
 
-let handleRemoveFromWatchlist = (movieObj) => {
+  let handleRemoveFromWatchlist = (movieObj) => {
     let filteredWatchlist = watchlist.filter((movie) => movie.id !== movieObj.id)
     setWatchlist(filteredWatchlist)
     localStorage.setItem("moviesApp", JSON.stringify(filteredWatchlist)) // 🔥 this line was missing
     console.log("Removed from watchlist:", filteredWatchlist)
   }
 
-useEffect(()=>{
-  let moviesFromLocalStorage=localStorage.getItem('moviesApp')
-  if(! moviesFromLocalStorage){
-    return 
-  }
-  setWatchlist(JSON.parse(moviesFromLocalStorage))
-},[])
+  useEffect(() => {
+    let moviesFromLocalStorage = localStorage.getItem('moviesApp')
+    if (!moviesFromLocalStorage) {
+      return
+    }
+    setWatchlist(JSON.parse(moviesFromLocalStorage))
+  }, [])
 
 
   return (
@@ -42,8 +45,8 @@ useEffect(()=>{
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path='/' element={<> <Banner/> <Movies watchlist={watchlist} handleAddToWatchlist={handleAddToWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} /></>}></Route>
-            <Route path='/watchlist' element={<Watchlist watchlist={watchlist}  setwatchlist={setWatchlist}/>}></Route>
+            <Route path='/' element={<> <Banner /> <Movies watchlist={watchlist} handleAddToWatchlist={handleAddToWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} /></>}></Route>
+            <Route path='/watchlist' element={<Watchlist watchlist={watchlist} setwatchlist={setWatchlist} />}></Route>
 
 
           </Routes>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,9 +4,9 @@ function Pagination({handlePrev,handleNext,pageNo}) {
   return (
     <div className='bg-gray-400 p-4 mt-8 flex justify-center gap-5'>
         
-        <div className='hover:cursor-pointer' onClick={handlePrev}><i class="fa-solid fa-arrow-left"></i></div>
+        <div className='hover:cursor-pointer' onClick={handlePrev}><i className="fa-solid fa-arrow-left"></i></div>
         <div className='font-bold'>{pageNo}</div>
-        <div className='hover:cursor-pointer' onClick={handleNext}><i class="fa-solid fa-arrow-right"></i></div>
+        <div className='hover:cursor-pointer' onClick={handleNext}><i className="fa-solid fa-arrow-right"></i></div>
     </div>
   )
 }
